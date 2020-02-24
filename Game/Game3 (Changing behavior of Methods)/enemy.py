b21b9b8bcdef9457dab39ef7d3e009b123c727c0
Vladimir Stoyanov <vgrozev@gmail.com>
2020-02-24 13:44:29 -0500

    adding more monsters

--- Game/Game3 (Changing behavior of Methods)/enemy.py
+++ Game/Game3 (Changing behavior of Methods)/enemy.py
@@ -3,7 +3,9 @@ class Enemy(object):
     def __init__(self, name="Enemy", lives=1, hit_points=0):
         self.name = name
         self.hit_points = hit_points
+        self.init_hit_points = hit_points       # preserving the original points, to reset to them when a life is lost
         self.lives = lives
+        self.alive = True
 
     def take_damage(self, damage):
         remaining_points = self.hit_points - damage
@@ -12,6 +14,12 @@ class Enemy(object):
             print("I took {} points damage and have {} left".format(damage, self.hit_points))
         else:
             self.lives -= 1
+            if self.lives > 0:
+                print("{0.name} lost a life".format(self))
+                self.hit_points = self.init_hit_points      # reset the hit_points for the next life if it is not 0
+            else:
+                print("{0.name} is dead".format(self))
+                self.alive = False
 
     def __str__(self):
         return "Name: {0.name}, Lives: {0.lives}, Hit points: {0.hit_points}".format(self)
--- Game/Game3 (Changing behavior of Methods)/main.py
+++ Game/Game3 (Changing behavior of Methods)/main.py
@@ -5,6 +5,8 @@ print("Ugly troll - {}".format(ugly_troll))
 
 another_troll = Troll("Ug")
 print("Another troll - {}".format(another_troll))
+another_troll.take_damage(18)
+print(another_troll)
 
 brother = Troll("Urg")
 print(brother)
@@ -13,6 +15,16 @@ ugly_troll.grunt()
 another_troll.grunt()
 brother.grunt()
 
+vamp = Vampire("Vlad")
+print(vamp)
+vamp.take_damage(5)
+print(vamp)
 
+print("-" * 40)
+another_troll.take_damage(30)
+print(another_troll)
 
+while vamp.alive:
+    vamp.take_damage(1)
+    print(vamp)
 
